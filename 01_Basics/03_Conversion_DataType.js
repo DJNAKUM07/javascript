309e34b39634b121c5f3ed11686f1f57a1f898e3
Digvijay Nakum <102287450+DJNAKUM07@users.noreply.github.com>
2023-08-08 17:54:47 +0000

    Conversion Of DataType

---
+++ 01_Basics/03_Conversion_DataType.js
@@ -0,0 +1,16 @@
+let score = 33;
+let score2 = "33"
+
+console.log(typeof score);
+console.log(typeof score2);
+
+let valueNumber = Number(score2);
+console.log(valueNumber);
+let temprature = null;
+console.log(Number(temprature)) // conversion give diff. value then actual.
+
+// "33" - 33
+// "33a" - NaN
+// true - 1
+// any Value to bool -  1
+//
