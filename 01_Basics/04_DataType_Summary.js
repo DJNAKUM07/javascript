7f614adab219fff38000cdbae97742bb6d66f5d1
Digvijay Nakum <102287450+DJNAKUM07@users.noreply.github.com>
2023-08-09 16:22:56 +0000

    Data Type Summary

---
+++ 01_Basics/04_DataType_Summary.js
@@ -0,0 +1,34 @@
+// 1.   Primitive Data Type
+
+// like - String , number , null , boolean ,Symbol,undefiend ,BigInt
+
+const name = 'DJ';
+const pass = 123;
+const pass2 = 123.3;
+let temp = null;
+const IsLogIn = false;
+
+// symbol - it is use to make value unique
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+const bigNumber = 12345678901234567n
+// 2.   Non Primitive. - reference
+
+// like
+// - Arrays , objects , functions
+
+const arr = ["abc" , "ced"];
+let myobj = {
+    name : "dj",
+    age : 99,
+}
+
+const myFunction = function(){
+    console.log("this is function call")
+}
+
+//console.log(myFunction());
+console.log(typeof myFunction);
