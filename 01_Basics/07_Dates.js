a7d4695155a38f8bb468f8f75cd31e4409c7dfdc
Digvijay Nakum <102287450+DJNAKUM07@users.noreply.github.com>
2023-10-21 18:09:49 +0000

    learn dates

---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,21 @@
+//Temporal.Now.instance(); - this will come in future
+
+let myDate = new Date(); //. object
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023,0,12); //  as  a array 0 - january
+
+// let date2 = new Date("10-21-2023")
+// console.log(myCreatedDate.toDateString());
+// console.log(date2.toLocaleString());
+
+let currDate = new Date();
+console.log(currDate);
+console.log(currDate.getDay());
+console.log(currDate.getMonth());
+
